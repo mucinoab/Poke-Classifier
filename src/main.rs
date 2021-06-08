225820b911ca2538b841e0e2a7594079e61ede07
Bruno A. Muciño <mucinoab@gmail.com>
2021-06-07 19:16:27 -0500

    Update dependencies

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,10 @@ use std::{
     time::{Duration, Instant},
 };
 
-use async_compat::Compat;
+use futures::StreamExt;
 use image::io::Reader as ImageReader;
 use inline_python::{python, Context};
 use serde::Deserialize;
-use smol::prelude::*;
 use telegram_bot::{
     prelude::*, reply_markup, Api, CanSendMessage, MessageKind, ParseMode, UpdateKind,
 };
@@ -18,7 +17,8 @@ use telegram_bot::{
 #[macro_use]
 extern crate log;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     log4rs::init_file("log_config.yml", Default::default()).expect("No se pudo iniciar Log");
     info!("Iniciando...");
 
@@ -42,124 +42,119 @@ fn main() {
 
     info!("Listo para recibir querys.");
 
-    smol::block_on(Compat::new(async {
-        while let Some(update) = stream.next().await {
-            match update {
-                Ok(update) => {
-                    if let UpdateKind::Message(mut message) = update.kind {
-                        let now = Instant::now();
-                        match message.kind {
-                            MessageKind::Photo { ref mut data, .. } => {
-                                write!(
-                                    telegram_url,
-                                    "bot{}/getFile?file_id={}",
-                                    &token, &data[0].file_id
-                                )
+    while let Some(update) = stream.next().await {
+        match update {
+            Ok(update) => {
+                if let UpdateKind::Message(mut message) = update.kind {
+                    let now = Instant::now();
+                    match message.kind {
+                        MessageKind::Photo { ref mut data, .. } => {
+                            write!(
+                                telegram_url,
+                                "bot{}/getFile?file_id={}",
+                                &token, &data[0].file_id
+                            )
+                            .unwrap();
+
+                            let response = client
+                                .get(&telegram_url)
+                                .send()
+                                .await
+                                .unwrap()
+                                .json::<Response>()
+                                .await
                                 .unwrap();
 
-                                let response = client
-                                    .get(&telegram_url)
-                                    .send()
-                                    .await
-                                    .unwrap()
-                                    .json::<Response>()
-                                    .await
-                                    .unwrap();
+                            telegram_url.truncate(telegram_len);
 
-                                telegram_url.truncate(telegram_len);
+                            write!(
+                                telegram_url,
+                                "file/bot{}/{}",
+                                &token, &response.result.file_path
+                            )
+                            .unwrap();
 
-                                write!(
-                                    telegram_url,
-                                    "file/bot{}/{}",
-                                    &token, &response.result.file_path
-                                )
+                            let image_bytes = client
+                                .get(&telegram_url)
+                                .send()
+                                .await
+                                .unwrap()
+                                .bytes()
+                                .await
                                 .unwrap();
 
-                                let image_bytes = client
-                                    .get(&telegram_url)
-                                    .send()
-                                    .await
-                                    .unwrap()
-                                    .bytes()
-                                    .await
-                                    .unwrap();
+                            let img = ImageReader::with_format(
+                                Cursor::new(image_bytes),
+                                image::ImageFormat::Jpeg,
+                            )
+                            .decode()
+                            .unwrap();
 
-                                let img = ImageReader::with_format(
-                                    Cursor::new(image_bytes),
-                                    image::ImageFormat::Jpeg,
-                                )
-                                .decode()
-                                .unwrap();
+                            img.resize_exact(680, 680, image::imageops::FilterType::Nearest);
 
-                                img.resize_exact(680, 680, image::imageops::FilterType::Nearest);
+                            img.save_with_format("i", image::ImageFormat::Jpeg).unwrap();
 
-                                img.save_with_format("i", image::ImageFormat::Jpeg).unwrap();
+                            c.run(python! {
+                                prediction = learn_inf.predict("i")
+                                ans = f"{prediction[0]}", f"{max(prediction[-1]):.4}"
+                            });
 
-                                c.run(python! {
-                                    prediction = learn_inf.predict("i")
-                                    ans = f"{prediction[0]}", f"{max(prediction[-1]):.4}"
-                                });
+                            ans = c.get::<Vec<String>>("ans");
 
-                                ans = c.get::<Vec<String>>("ans");
+                            let pokelink = format!("https://www.pokemon.com/us/pokedex/{}", ans[0]);
 
-                                let pokelink =
-                                    format!("https://www.pokemon.com/us/pokedex/{}", ans[0]);
+                            write!(
+                                &mut text,
+                                "[{}]({}), confidence {}",
+                                ans[0], pokelink, ans[1],
+                            )
+                            .unwrap();
 
-                                write!(
-                                    &mut text,
-                                    "[{}]({}), confidence {}",
-                                    ans[0], pokelink, ans[1],
-                                )
-                                .unwrap();
+                            text[..=1].make_ascii_uppercase();
 
-                                text[..=1].make_ascii_uppercase();
+                            let mut reply = message.text_reply(&text);
+                            reply.parse_mode(ParseMode::Markdown);
+                            reply.reply_markup(
+                                reply_markup!(inline_keyboard, ["Pokédex" url pokelink]),
+                            );
 
-                                let mut reply = message.text_reply(&text);
-                                reply.parse_mode(ParseMode::Markdown);
-                                reply.reply_markup(
-                                    reply_markup!(inline_keyboard, ["Pokédex" url pokelink]),
-                                );
+                            api.spawn(reply);
 
-                                api.spawn(reply);
+                            telegram_url.truncate(telegram_len);
+                            text.clear();
+                        }
 
-                                telegram_url.truncate(telegram_len);
-                                text.clear();
-                            }
-
-                            MessageKind::Text { ref mut data, .. } => {
-                                if data.contains("/h") {
-                                    api.spawn(message.chat.text(HELP));
-                                } else if data.contains("/s") {
-                                    api.spawn(
-                                        message.chat.text(START).parse_mode(ParseMode::Markdown),
-                                    );
-                                } else {
-                                    api.spawn(message.chat.text(NO_MATCH));
-                                }
-                            }
-
-                            _ => {
+                        MessageKind::Text { ref mut data, .. } => {
+                            if data.contains("/h") {
+                                api.spawn(message.chat.text(HELP));
+                            } else if data.contains("/s") {
+                                api.spawn(message.chat.text(START).parse_mode(ParseMode::Markdown));
+                            } else {
                                 api.spawn(message.chat.text(NO_MATCH));
                             }
                         }
 
-                        info!(
-                            "{} {} {} {:#?}",
-                            &message.from.first_name,
-                            ans[0],
-                            ans[1],
-                            Instant::now().duration_since(now)
-                        );
+                        _ => {
+                            api.spawn(message.chat.text(NO_MATCH));
+                        }
                     }
-                }
 
-                Err(e) => {
-                    error!("{}", e);
-                    thread::sleep(Duration::from_secs(1));
+                    info!(
+                        "{} {} {} {:#?}",
+                        &message.from.first_name,
+                        ans[0],
+                        ans[1],
+                        Instant::now().duration_since(now)
+                    );
                 }
             }
+
+            Err(e) => {
+                error!("{}", e);
+                thread::sleep(Duration::from_secs(1));
+            }
         }
-    }));
+    }
 }
 
 #[derive(Deserialize)]
